d37ade39d84c66fbf7a618abd516bf5ab20f2eca
Preethi Kotaru <preethi.kotaruu@gmail.com>
2026-08-25 21:19:26 -0500

    feat: implement user authentication

---
+++ backend/src/main/java/com/planora/backend/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.planora.backend.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
