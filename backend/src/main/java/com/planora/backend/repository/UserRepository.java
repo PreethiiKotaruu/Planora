cbde93437bc235b93b69638f650ffef501c3ca6e
Preethi Kotaru <preethi.kotaruu@gmail.com>
2026-08-25 21:19:05 -0500

    feat: add login request and user lookup

--- backend/src/main/java/com/planora/backend/repository/UserRepository.java
+++ backend/src/main/java/com/planora/backend/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.planora.backend.repository;
 
+import java.util.Optional;
 import com.planora.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email); //Reason for using optional:There may or may not be a User result
+
 }
